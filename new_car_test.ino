ff455182ede5780ab3c23eeeba4b619b47498238
TheMadGrammer <blueberryman2112@gmail.com>
2018-12-26 14:46:27 -0500

    Inital Commit

---
+++ new_car_test.ino
@@ -0,0 +1,16 @@
+#define rightForward 8
+#define rightBackward 12
+#define leftForward 13
+#define leftBackward 11
+#define rightSpeed 9
+#define leftSpeed 10
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+ 
+
+}
