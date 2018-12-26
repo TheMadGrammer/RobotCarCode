cbea91fd39b654e6d2c5262e03617d56f3e2021b
TheMadGrammer <blueberryman2112@gmail.com>
2018-12-26 16:07:09 -0500

    Testing for motor functionality

--- new_car_test.ino
+++ new_car_test.ino
@@ -1,7 +1,7 @@
-#define rightForward 8
-#define rightBackward 12
-#define leftForward 13
-#define leftBackward 11
+#define rightBackward 8
+#define rightForward 12
+#define leftBackward 13
+#define leftForward 11
 #define rightSpeed 9
 #define leftSpeed 10
 
@@ -11,6 +11,19 @@ void setup() {
 }
 
 void loop() {
- 
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, LOW);
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftBackward, LOW);
+  analogWrite(rightSpeed, 45);
+  analogWrite(leftSpeed, 45);
 
+  delay(5000);
+
+  digitalWrite(rightForward, LOW);
+  digitalWrite(leftForward, LOW);
+  analogWrite(rightSpeed, 0);
+  analogWrite(leftSpeed, 0);
+
+  delay(5000);
 }
